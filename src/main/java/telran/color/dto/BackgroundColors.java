9cc15d1539a0d86b89aa1cee151cbf8761a52eea
Dina <presler1990@gmail.com>
2021-12-07 12:27:45 +0200

    dff

--- src/main/java/telran/color/dto/BackgroundColors.java
+++ src/main/java/telran/color/dto/BackgroundColors.java
@@ -1,17 +1,21 @@
 package telran.color.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 
 public class BackgroundColors {
-String closest_palette_color;
+	@JsonAlias({"closest_palette_color"})
+String closestPaletteColor;
+	
 String closest_palette_color_parent;
 String percent;
 @Override
 public String toString() {
-	return closest_palette_color  +"\t"+ 
+	return closestPaletteColor  +"\t"+ 
 			 closest_palette_color_parent + "\t" +"\t" + percent;
 }
 }
